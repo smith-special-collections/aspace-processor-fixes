0a01882879368567bd50685f7fc776ddf1647d6a
Dave Mayo <dave_mayo@harvard.edu>
2016-06-13 13:05:08 -0400

    Fixed "no #content on nil" error on last line

--- didm-1a.rb
+++ didm-1a.rb
@@ -1,5 +1,6 @@
 fix_for 'didm-1a', depends_on: ['noempty-1']  do
   eadid = @xml.at_xpath('/ead/eadheader/eadid')
-  unitid = eadid['identifier'] || eadid.content
-  @xml.at_xpath('/ead/archdesc/did/unitid').content = unitid
+  unitid_content = eadid['identifier'] || eadid.content
+  unitid = @xml.at_xpath('/ead/archdesc/did/unitid') || @xml.at_xpath('/ead/archdesc/did').add_child('<unitid />').first
+  unitid.content = unitid_content
 end
