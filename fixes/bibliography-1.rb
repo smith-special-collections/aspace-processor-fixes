59122d8f44aae5801da47125e9b4011e1fe72701
Dave Mayo <dave_mayo@harvard.edu>
2016-09-13 14:06:24 -0400

    Whoops, noko elements are iterables, the meaningful change is at_xpath -> xpath

--- fixes/bibliography-1.rb
+++ fixes/bibliography-1.rb
@@ -1,17 +1,18 @@
 # Rewrite bibliography with nested list as bibliography with series of bibrefs
 fix_for 'bibliography-1', depends_on: ['noempty-1'] do
   @xml.xpath('//bibliography[./list]').each do |bib|
-    bib.at_xpath('./list').replace(
-      Nokogiri::XML::NodeSet.new(@xml,
-                                 bib.at_xpath('./list/item').map do |item|
-                                   if item.element_children.count == 1 && item.first_element_child.name =='bibref'
-                                     item.first_element_child
-                                   else
-                                     item.name = 'bibref'
-                                     item
-                                   end
-                                 end
-                                )
-    )
+    # turn everything into bibrefs
+    items = Nokogiri::XML::NodeSet.new(@xml,
+                                       bib.xpath('./list/item').map do |item|
+                                         if item.element_children.count == 1 && item.first_element_child.name =='bibref'
+                                           item.first_element_child
+                                         else
+                                           item.name = 'bibref'
+                                           item
+                                         end
+                                       end
+                                      )
+    # replace list with bibrefs
+    bib.at_xpath('./list').replace(items)
   end
 end
