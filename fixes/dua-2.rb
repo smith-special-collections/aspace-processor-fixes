87d670ca412eee06db02863b0ffceeb7fc3f6a45
Jay <surenderjamplu@gmail.com>
2016-10-04 16:41:21 -0400

    improved regexp to strip circa appro etc.,

--- fixes/dua-2.rb
+++ fixes/dua-2.rb
@@ -4,12 +4,12 @@ fix_for "dua-2", depends_on: ['noempty-1'] do
      contains(translate(./text(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "ca") or
      contains(translate(./text(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "ca.") or
      contains(translate(./text(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "c.") or
-     starts-with(translate(./text(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "c.") or
+     contains(translate(./text(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "c ") or
      contains(translate(./text(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "approximately") or
      contains(translate(./text(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "approx"))
       and @certainty != "approximate"]').each do |ud|
       a = ud.text
-      a = a.gsub!(/\b(?:c(?:a)|circa|c.|ca|approx(?:imately)?|\?)[,.]?/, '').gsub(/[\[\]]/, '').strip.gsub(/\s+/, ' ')
+      a = a.gsub!(/\b(circa|ca?|approx(imately)?)\b[,.]?/, '').gsub(/[\[\]]/, '').strip.gsub(/\s+/, ' ')
       ud.content = a
   end
 end
