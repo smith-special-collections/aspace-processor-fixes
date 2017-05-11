55c432b6477db0095bd4811c8117201a80ad657a
Dave Mayo <pobocks@gmail.com>
2017-05-10 20:12:20 -0400

    Alter c-1 handling to take into account leveled cs

--- fixes/c-1.rb
+++ fixes/c-1.rb
@@ -1,5 +1,8 @@
 fix_for 'c-1', depends_on: ['noempty-1'] do
-  @xml.xpath("//c[not(@level) or (@level = 'otherlevel' and not(@otherlevel))]").each do |c|
+  c_level = 'c00'
+  preposition = "[not(@level) or (@level = 'otherlevel' and not(@otherlevel))]"
+  path = "//c#{preposition}|" + 11.times.map { "//#{c_level.succ! + preposition}" }.join("|")
+  @xml.xpath(path).each do |c|
     c['level'] = 'otherlevel' unless c['level']
     if c['level'] == 'otherlevel'
       c['otherlevel'] = c['otherlevel'] || 'unspecified'
