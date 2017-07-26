b5c8bbe27195417c167becc1b99e15a27f227eb5
Dave Mayo <pobocks@gmail.com>
2017-07-26 13:00:09 -0400

    Smith customizations

--- fixes/da-3.rb
+++ fixes/da-3.rb
@@ -1,16 +1,17 @@
 # move descgrp subelements of various types to parent of descgrp, remove descgrp if empty
 fix_for 'da-3', depends_on: ['noempty-1']  do
-  path = "//descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/accessrestrict|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/accruals|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/acqinfo|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/altformavail|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/appraisal|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/custodhist|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/note|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/prefercite|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/processinfo|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/separatedmaterial|
-          //descgrp[(@type and @type != 'add') or (not(normalize-space(@type)) and @encodinganalog = '544')]/userestrict"
+  path = "//descgrp/accessrestrict|
+          //descgrp/accruals|
+          //descgrp/acqinfo|
+          //descgrp/altformavail|
+          //descgrp/appraisal|
+          //descgrp/custodhist|
+          //descgrp/note|
+          //descgrp/prefercite|
+          //descgrp/processinfo|
+	  //descgrp/relatedmaterial|
+          //descgrp/separatedmaterial|
+          //descgrp/userestrict"
   @xml.xpath(path).each do |el|
     descgrp = el.parent
     el.parent = descgrp.parent
--- fixes/unittitle-3.rb
+++ fixes/unittitle-3.rb
@@ -1,10 +1,24 @@
 fix_for 'unittitle-3', depends_on: ['noempty-1', 'noempty-2', 'dsc-duped-cs'] do
+  drop_me = []
   @xml.xpath('//unittitle[.//unitdate]').each do |ut|
     # Because we're moving uds into following-sibling position of ut,
     #   process unitdates in reverse
+
     ut.xpath('.//unitdate').reverse.each do |ud|
       ut.add_next_sibling(ud.dup)
-      ud.replace(Nokogiri::XML::Text.new(ud.text, @xml))
+      if ud.xpath('./following-sibling::node()').to_s[/\A,?\s*\z/]
+        ud.xpath('./following-sibling::node()').remove
+        ud.remove
+      else
+        ud.replace(Nokogiri::XML::Text.new(ud.text, @xml))
+      end
     end
+    if ut.content.strip.empty?
+      drop_me << ut
+    end
+  end
+
+  drop_me.each do |ut|
+    ut.remove
   end
 end
